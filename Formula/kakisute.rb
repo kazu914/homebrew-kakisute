a018685e1bf5abe6efd206f1622940410b54d835
kazu914 <nkz0914ssk@gmail.com>
2022-11-19 23:51:41 +0900

    update tarball information

--- Formula/kakisute.rb
+++ Formula/kakisute.rb
@@ -2,10 +2,10 @@ class Kakisute < Formula
   desc "Kakisute(scribbled snippets) management tool written in Rust"
   homepage "https://github.com/kazu914/kakisute"
   url "https://github.com/kazu914/kakisute/releases/download/v0.1.2/kakisute-x86_64-apple-darwin.tar.gz"
-  sha256 "ab5cb92a32e73d3940e5e5add480d11fb324437f559ea917371d0c20fc30a0cb"
+  sha256 "ffaaa6046a7105c97257a3e58d3ddca2292ea901eeb871391467ea0fe0928bca"
   version "0.1.2"
 
   def install
-    bin.install "target/x86_64-apple-darwin/release/kakisute" => "kakisute"
+    bin.install "kakisute"
   end
 end
