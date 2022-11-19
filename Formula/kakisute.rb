6472e5faf9168d3f2d37ab5ca27dda7630da3b17
kazu914 <nkz0914ssk@gmail.com>
2022-11-20 01:07:16 +0900

    add complete script

--- Formula/kakisute.rb
+++ Formula/kakisute.rb
@@ -2,10 +2,17 @@ class Kakisute < Formula
   desc "Kakisute(scribbled snippets) management tool written in Rust"
   homepage "https://github.com/kazu914/kakisute"
   url "https://github.com/kazu914/kakisute/releases/download/v0.1.2/kakisute-x86_64-apple-darwin.tar.gz"
-  sha256 "ffaaa6046a7105c97257a3e58d3ddca2292ea901eeb871391467ea0fe0928bca"
+  sha256 "cb2b6a049bf644e11fbd4643509762320520c66f395bc682115d10f2e0189595"
   version "0.1.2"
 
   def install
     bin.install "kakisute"
+
+    # bash completion
+    output = Utils.safe_popen_read("#{bin}/kakisute", 'completion', '--shell', 'bash')
+    (bash_completion / 'kakisute').write output
+    # zsh completion
+    output = Utils.safe_popen_read("#{bin}/kakisute", 'completion', '--shell', 'zsh')
+    (zsh_completion / '_kakisute').write output
   end
 end
